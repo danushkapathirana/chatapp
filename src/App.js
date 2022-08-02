924dd0a2747606ce20571a44cc6eb3fcd7c50ca8
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2022-08-02 22:11:58 +0530

    developed primary nav and basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+
+const primaryNavigation = ["Home", "Chat", "Contact Us", "Sign In"]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="chat-app">
+      <header className="header">
+        <div className='logo'>
+          <i class="fa-solid fa-comments"></i>
+        </div>
+        <nav className='primary-nav'>
+          <ul className='nav-items'>
+            {primaryNavigation.map((item) =>
+              <li className='nav-item' key={item}>{item}</li>
+            )}
+          </ul>
+        </nav>
       </header>
+
+      <div className='app-body'>
+        <div className='chat-wrapper'>
+          <div className='chat-container'>
+            <div className='col col-1 main-navigation'></div>
+            <div className='col col-2 users-list'></div>
+            <div className='col col-3 chat-room'></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
